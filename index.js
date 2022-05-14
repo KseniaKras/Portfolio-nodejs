e137386bcd9b71bc7033015579612587d45d7450
Ksenia <ksenia.kras29@gmail.com>
2022-05-14 18:17:13 +0300

    refactor

--- index.js
+++ index.js
@@ -48,8 +48,9 @@ app.post('/send', async function (req, res) {
     res.send("ok")
 })
 
-let port = process.env.PORT || 3010
+// let port = process.env.PORT || 3010
 
-app.listen(port, function () {
-    console.log('Example app listening on port 3010!')
+const server = app.listen(process.env.PORT || 5000, () => {
+    const port = server.address().port;
+    console.log(`Express is working on port ${port}`);
 })
