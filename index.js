199fae3465b7dbd70ed9e5677544ed10e592f48b
Ksenia <ksenia.kras29@gmail.com>
2022-05-14 18:29:48 +0300

    debug

--- index.js
+++ index.js
@@ -48,9 +48,8 @@ app.post('/send', async function (req, res) {
     res.send("ok")
 })
 
-// let port = process.env.PORT || 3010
-
 const server = app.listen(process.env.PORT || 5000, () => {
     const port = server.address().port;
+    console.log(server.address())
     console.log(`Express is working on port ${port}`);
 })
